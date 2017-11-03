4067f97305ed3b5e54a6f84c563fc42bd11dc956
Aya Sugawara <xexist@gmail.com>
2017-11-03 18:11:20 +0900

    no message

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -48,13 +48,17 @@ var app = {
     }
 };
 
-/* var */
+/* -----------------------------------------------------
+ * var
+ * ----------------------------------------------------*/
 // Layout Elements
 var title = document.getElementById('title');
 var photo = document.getElementById('photo');
 var photo1 = document.getElementById('photo1');
 var photo2 = document.getElementById('photo2');
 var filter = document.getElementById('filter');
+var blendMode = document.getElementById('blendMode');
+var loader = document.getElementById('loader');
 var menu = document.getElementById('menu');
 
 // Button Elements
@@ -62,45 +66,46 @@ var btnHome = document.getElementById('btnHome');
 var btnBack = document.getElementById('btnBack');
 var btnNext = document.getElementById('btnNext');
 var btnFilter = document.getElementById('btnFilter');
+var btnTint = document.getElementById('btnTint');
 var btnGallery = document.getElementById('btnGallery');
 
-/* EventListener */
+// 
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+var windowAspect = window.innerWidth / window.innerHeight;
+
+/* -----------------------------------------------------
+ * Btn
+ * ----------------------------------------------------*/
+//========== Home ==========
 btnHome.addEventListener('touchstart', function (e) {
-  selectedPhoto.setAttribute('src', '');
+//  selectedPhoto.setAttribute('src', '');
 });
+//========== Back ==========
 btnBack.addEventListener('touchstart', function (e) {
 });
+//========== Next ==========
 btnNext.addEventListener('touchstart', function (e) {
 });
+//========== Filter ==========
 btnFilter.addEventListener('touchstart', function (e) {
+    loader.style.display='none';
     if (filter.style.display == '') {
       filter.style.display='block';
     } else {
       filter.style.display='';
     }
 });
-btnGallery.addEventListener('touchstart', function (e) {
-    navigator.camera.getPicture(function(imageURI){
-      photo.width = imageURI.width;
-      photo.width = imageURI.height;
-      photo1.setAttribute('src', imageURI);
-    }, function(msg){
-    }, {
-      quality:80,
-      destinationType:Camera.DestinationType.FILE_URI,
-      sourceType:Camera.PictureSourceType.SAVEDPHOTOALBUM,
-    });
-});
-
-
 // MyEventListener オブジェクト作成
 var MyEventListener = function() { return this }
 // MyEventListener オブジェクトにonMouseClickメソッドを追加
 MyEventListener.prototype = {
   onMouseClick: function() { 
-    photo2.setAttribute('src', this.getAttribute('src'));
+    var img = this.getAttribute('src');
+    photo2.style.backgroundImage = 'url("' + img + '")';
+//    photo2.src = img;
     filter.style.display='';
-  }
+  } 
 }
 // MyEventListener から myEventListener オブジェクトを作成
 var myEventListener = new MyEventListener();
@@ -108,3 +113,89 @@ var filters = filter.children;
 for (var i = 0; i < filters.length; i++){
     filters[i].addEventListener("click", myEventListener.onMouseClick, false);
 }
+//========== Tint ==========
+btnTint.addEventListener('touchstart', function (e) {
+    loader.style.display='none';
+    if (blendMode.style.display == '') {
+      blendMode.style.display='block';
+    } else {
+      blendMode.style.display='';
+    }
+});
+// MyEventListener オブジェクト作成
+var MyEventListener = function() { return this }
+// MyEventListener オブジェクトにonMouseClickメソッドを追加
+MyEventListener.prototype = {
+  onMouseClick: function() { 
+    var id = this.id;
+    console.log(id);
+    photo2.setAttribute('class', id);
+    blendMode.style.display='';
+  }
+}
+// MyEventListener から myEventListener オブジェクトを作成
+var myEventListener = new MyEventListener();
+var blendModeChild = blendMode.children;
+for (var i = 0; i < blendModeChild.length; i++){
+    blendModeChild[i].addEventListener("click", myEventListener.onMouseClick, false);
+}
+//========== Gallerry ==========
+btnGallery.addEventListener('touchstart', function (e) {
+    loader.style.display='block';
+    loader.setAttribute('class', 'fade');
+    var imgSrc;
+    var getPic = function(done) {
+      return new Promise(function(resolve, reject) {
+        navigator.camera.getPicture(function(imgUri){
+          var img = new Image();
+//          img.src = 'data:image/jpeg;base64,' + imgUri;
+          img.src = imgUri;
+          img.onload = function() {
+            var imgWidth = img.width;
+            var imgHeight = img.height;
+            var imgAspect = imgWidth / imgHeight;
+
+            photo1.width = windowWidth;
+ 
+//            alert(imgAspect +'　'+ windowAspect +'　'+imgWidth +'　'+windowWidth);
+            imgSrc = img.src;
+            photo1.src = img.src;
+            resolve();
+          }
+        }, function(msg){
+          setTimeout("loader.setAttribute('class', '')", 1000);
+        }, {
+          quality:60,
+          destinationType:Camera.DestinationType.DATA_URI,
+//          destinationType:Camera.DestinationType.DATA_URI,
+          sourceType:Camera.PictureSourceType.SAVEDPHOTOALBUM,
+        });
+      });
+    };
+    getPic()
+      .then(function() {
+        var timer = setInterval(function() {
+          if( imgSrc == photo1.getAttribute('src') ){
+            clearInterval(timer);
+            loader.setAttribute('class', '');
+//            alert(photo1.width +'　'+ photo1.height);
+            var w1 = photo1.width + 'px';
+            photo2.style.width = w1;
+//            photo2.width = photo1.width;
+            photo2.style.height = photo1.height + 'px';
+     position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+
+/*
+            photo2.style.width = photo1.width + 'px';
+            photo2.style.height = photo1.height + 'px';
+*/
+          }
+        },1000);
+      })
+});
+
